25ea8fb68fd006b0d404aee57c100cf9b3663272
Ashwath Kumaran <ashwath.kumaran@psiog.com>
2025-03-18 18:06:43 +0530

    mixing pandas and numpy

--- Pandas.py
+++ Pandas.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import numpy as np
 
-s1= pd.Series([1,2,3,4,5])
-print(s1)
+# s1= pd.Series([1,2,3,4,5])
+# s2= pd.Series([1,2,3,4,5], index=['a','b','c','d','e'], name='sample_series')
+# s3= s1[s1>3]
+# s4= s1*2
+# print(s4)
+
+arr = np.array([3.6,"1", 2.8, True, 4, 5])
+s = pd.Series(arr)
+print(s)
