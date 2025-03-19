e86e0ad5de40f1f09f2d0fd076b94b8c6c1c1bfe
Ashwath Kumaran <ashwath.kumaran@psiog.com>
2025-03-19 11:16:15 +0530

    Completing the pandas

--- Pandas.py
+++ Pandas.py
@@ -7,6 +7,30 @@ import numpy as np
 # s4= s1*2
 # print(s4)
 
-arr = np.array([3.6,"1", 2.8, True, 4, 5])
-s = pd.Series(arr)
-print(s)
+# arr = np.array([3.6,"1", 2.8, True, 4, 5])
+# index = ['a','b','c','d','e','f']
+# s = pd.Series(arr, index=index)
+# print(s.size)
+
+# Data frame
+s1= pd.Series(['Ash','Ash','Ash','Ash','Ash'])
+s2= pd.Series([1,2,3,4,5])
+df = pd.DataFrame({'Name':s1, 'Rank':s2})
+# print(df)
+
+# for (row_index, row_value) in df.iterrows():
+#     # print(row_index, row_value)
+#     print(row_value['Name'], row_value['Rank'])
+
+dict1 = {
+          'Name':['Ash','Ash','Ash','Ash','Ash'],
+          'Rank':[1,2,3,4,5]
+        }
+dict2 = {
+          'Name':['Mal','Mal','Mal'],
+          'Rank':[1,2,3]
+        }
+df = pd.DataFrame(dict1)
+df2 = pd.DataFrame(dict2)
+df3 = pd.concat([df,df2], axis=0)
+print(df3)
